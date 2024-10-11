d9bcb82979fa151758e742ac150cc4d018c5cc0b
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2024-10-11 10:37:04 +0800

    update conway animation

--- conway.py
+++ conway.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 from lib import fft_convolve2d
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, PillowWriter
 plt.ion()
 
 def conway(state, k=None):
@@ -10,34 +11,44 @@ def conway(state, k=None):
     """
 
     # set up kernel if not given
-    if k == None:
+    if k is None:
         m, n = state.shape
         k = np.zeros((m, n))
-        k[m/2-1 : m/2+2, n/2-1 : n/2+2] = np.array([[1,1,1],[1,0,1],[1,1,1]])
+        k[m//2-1 : m//2+2, n//2-1 : n//2+2] = np.array([[1,1,1],[1,0,1],[1,1,1]])
 
     # computes sums around each pixel
-    b = fft_convolve2d(state,k).round()
+    b = fft_convolve2d(state, k).round()
     c = np.zeros(b.shape)
 
-    c[np.where((b == 2) & (state == 1))] = 1
-    c[np.where((b == 3) & (state == 1))] = 1
-
-    c[np.where((b == 3) & (state == 0))] = 1
+    c[(b == 2) & (state == 1)] = 1
+    c[(b == 3) & (state == 1)] = 1
+    c[(b == 3) & (state == 0)] = 1
 
     # return new state
     return c
 
 if __name__ == "__main__":
     # set up board
-    m,n = 100,100
-    A = np.random.random(m*n).reshape((m, n)).round()
+    m, n = 100, 100
+    A = np.random.random((m, n)).round()
 
-    # plot each frame
-    plt.figure()
-    img_plot = plt.imshow(A, interpolation="nearest", cmap = plt.cm.gray)
-    plt.show(block=False)
-    while True:
+    # set up the figure and animation
+    fig, ax = plt.subplots()
+    img_plot = ax.imshow(A, interpolation="nearest", cmap=plt.cm.gray)
+    
+    def update(frame):
+        global A
         A = conway(A)
-        img_plot.set_data(A)
-        plt.draw()
-        time.sleep(0.01)
+        img_plot.set_array(A)
+        return [img_plot]
+    
+    # create the animation
+    anim = FuncAnimation(fig, update, frames=200, interval=50, blit=True)
+    
+    # save the animation as a gif
+    writer = PillowWriter(fps=25)
+    anim.save("conway_game_of_life.gif", writer=writer)
+    
+    print("GIF saved as 'conway_game_of_life.gif'")
+    
+    plt.show()
